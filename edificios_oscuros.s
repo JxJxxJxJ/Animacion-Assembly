5eafab074144eb42113577369ca036f12de9580a
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-05-25 16:17:40 -0300

    edif oscuros bien avanzados.

--- edificios_oscuros.s
+++ edificios_oscuros.s
@@ -30,6 +30,45 @@ edificios_oscuros:
     LDR X7, =FONDO_VARANDA_0
     BL dibujar_rectangulo
 
+    // ventanas de la base
+    // hago los cuadrados celestes (ventanas)
+    MOV X1, 423
+    MOV X2, 225
+    MOV X3, 455
+    MOV X4, 256
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    // sobrepinto los marcos
+    MOV X1, 434
+    MOV X2, 225
+    MOV X3, 437
+    MOV X4, 256
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 445
+    MOV X2, 225
+    MOV X3, 448
+    MOV X4, 256
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 423
+    MOV X2, 230
+    MOV X3, 455
+    MOV X4, 238
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+    
+    MOV X1, 423
+    MOV X2, 242
+    MOV X3, 455
+    MOV X4, 251
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+
     // edificio 1
     MOV X1, 472
     MOV X2, 165
@@ -79,43 +118,64 @@ edificios_oscuros:
 
     // ventanas edif 1
     // hago los cuadrados celestes (ventanas)
-    MOV X1, 423
-    MOV X2, 225
-    MOV X3, 455
-    MOV X4, 256
+    MOV X1, 476
+    MOV X2, 178
+    MOV X3, 505
+    MOV X4, 221
     LDR X7, =DARKSLATEGRAY_3
     BL dibujar_rectangulo
 
     // sobrepinto los marcos
-    MOV X1, 434
-    MOV X2, 225
-    MOV X3, 437
-    MOV X4, 256
-    LDR X7, =FONDO_VARANDA_0
-    BL dibujar_rectangulo
-
-    MOV X1, 445
-    MOV X2, 225
-    MOV X3, 448
-    MOV X4, 256
-    LDR X7, =FONDO_VARANDA_0
-    BL dibujar_rectangulo
-
-    MOV X1, 423
-    MOV X2, 230
-    MOV X3, 455
-    MOV X4, 238
-    LDR X7, =FONDO_VARANDA_0
-    BL dibujar_rectangulo
     
-    MOV X1, 423
-    MOV X2, 242
-    MOV X3, 455
-    MOV X4, 251
+    MOV X1, 484
+    MOV X2, 178
+    MOV X3, 487
+    MOV X4, 221
     LDR X7, =FONDO_VARANDA_0
     BL dibujar_rectangulo
 
+    MOV X1, 495
+    MOV X2, 178
+    MOV X3, 498
+    MOV X4, 221
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
 
+    MOV X1, 476
+    MOV X2, 183
+    MOV X3, 505
+    MOV X4, 189
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 476
+    MOV X2, 195
+    MOV X3, 505
+    MOV X4, 204
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 476
+    MOV X2, 208
+    MOV X3, 505
+    MOV X4, 216
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 475
+    MOV X2, 220
+    MOV X3, 476
+    MOV X4, 221
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 483
+    MOV X2, 220
+    MOV X3, 484
+    MOV X4, 221
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+  
     // conexion edif 1 y 2
     MOV X1, 512
     MOV X2, 205
@@ -123,7 +183,7 @@ edificios_oscuros:
     MOV X4, 298
     LDR X7, =FONDO_VARANDA_0
     BL dibujar_rectangulo
-
+ 
 
     // edificio 2
 
@@ -134,6 +194,12 @@ edificios_oscuros:
     LDR X7, =FONDO_VARANDA_0
     BL dibujar_rectangulo
 
+    MOV X1, 575
+    MOV X2, 112
+    MOV X3, 573
+    MOV X4, 101
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
 
     // luces
     MOV X1, 537
@@ -149,6 +215,71 @@ edificios_oscuros:
     MOV X4, 112
     LDR X7, =SALMON
     BL dibujar_rectangulo
+
+
+    // ventanas edif 2
+    // hago los cuadrados celestes (ventanas)
+    MOV X1, 537
+    MOV X2, 126
+    MOV X3, 579
+    MOV X4, 167
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    MOV X1, 537
+    MOV X2, 178
+    MOV X3, 567
+    MOV X4, 182
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    // sobrepinto los marcos
+    
+    MOV X1, 545
+    MOV X2, 126
+    MOV X3, 549
+    MOV X4, 182
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 557
+    MOV X2, 126
+    MOV X3, 560
+    MOV X4, 182
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 568
+    MOV X2, 126
+    MOV X3, 571
+    MOV X4, 182
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 537
+    MOV X2, 131
+    MOV X3, 579
+    MOV X4, 137
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 537
+    MOV X2, 143
+    MOV X3, 579
+    MOV X4, 150
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    MOV X1, 537
+    MOV X2, 156
+    MOV X3, 579
+    MOV X4, 164
+    LDR X7, =FONDO_VARANDA_0
+    BL dibujar_rectangulo
+
+    
+
+
 
     // edificio 3
 
@@ -196,6 +327,49 @@ edificios_oscuros:
     LDR X7, =DARKSLATEGRAY_3
     BL dibujar_rectangulo
 
+
+    MOV X1, 587
+    MOV X2, 187
+    MOV X3, 590
+    MOV X4, 194
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    MOV X1, 595
+    MOV X2, 187
+    MOV X3, 599
+    MOV X4, 194
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    MOV X1, 602
+    MOV X2, 186
+    MOV X3, 606
+    MOV X4, 194
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+    MOV X1, 611
+    MOV X2, 186
+    MOV X3, 614
+    MOV X4, 194
+    LDR X7, =DARKSLATEGRAY_3
+    BL dibujar_rectangulo
+
+
+    MOV X1, 604
+    MOV X2, 187
+    MOV X3, 606
+    MOV X4, 194
+    LDR X7, =STEELBLUE_2
+    BL dibujar_rectangulo
+
+    MOV X1, 611
+    MOV X2, 187
+    MOV X3, 613
+    MOV X4, 194
+    LDR X7, =STEELBLUE_2
+    BL dibujar_rectangulo
 
     // sobrepinto los marcos
     MOV X1, 576
