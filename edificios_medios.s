68d06d229432f343f185220df42e59b1464657e4
Nehuen1304 <nehuen.guevara@mi.unc.edu.ar>
2024-05-25 21:26:42 -0300

    Primer edificio medio

--- edificios_medios.s
+++ edificios_medios.s
@@ -1,11 +1,11 @@
-.ifndef EDIFICIOS_CLAROS_S
-.equ EDIFICIOS_CLAROS_S, 0x000000
+.ifndef EDIFICIOS_MEDIOS_S
+    .equ EDIFICIOS_MEDIOS_S, 0x000000
 
 .include "colores.s"
 .include "framebuffer.s"
 .include "elementales.s"
 
-edificios_claros:
+edificios_medios:
     // PUSH(X30)
     SUB SP, SP, 48
     STR X1, [SP]
@@ -13,15 +13,21 @@ edificios_claros:
     STR X3, [SP, 16]
     STR X4, [SP, 24]
     STR X7, [SP, 32]
-    STR X30, [SP, 40]    
+    STR X30, [SP, 40]
+
+    // -------------------------------
+    // EDIFICIO 1
+    // -------------------------------
 
 
-    // MOV X1, // ... 
-    // MOV X2, // ...
-    // MOV X3, // ...
-    // MOV X4, // ...
-    // LDR X7, =STEELBLUE_2
-    // BL dibujar_rectangulo
+    // Edificio 1 torre más alta
+
+    MOV X1, 310
+    MOV X2, 171
+    MOV X3, 278
+    MOV X4, 171
+    LDR X7, = DARKSLATEGRAY_3
+    BL dibujar_rectangulo
 
 
     // PUSH(X30)
@@ -30,8 +36,9 @@ edificios_claros:
     LDR X4, [SP, 24]
     LDR X3, [SP, 16]
     LDR X2, [SP, 8]
-    LDR X1, [SP] 
+    LDR X1, [SP]
     ADD SP, SP, #48
-RET
-.endif
 
+    RET
+
+.endif
